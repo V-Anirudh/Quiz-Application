b16e29efbf5bce6b8c0505436cb4e0a2f39c9fd3
V-Anirudh <vallampatlaanirudh@gmail.com>
2021-07-17 11:46:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-
 from flask import Flask, render_template, request, session, redirect
 import pymongo
 import jinja2
 import bcrypt
-
 
 app = Flask(__name__)
 # MongoDb Connection
@@ -13,19 +11,16 @@ my_client = pymongo.MongoClient(connection_String)
 db = my_client.db_quiz
 Quiz = []
 
-@app.route('/', methods=["GET", "POST"])
-@app.route('/index/')
 
+@app.route('/', methods=["GET", "POST"])
+
+@app.route('/index/')
 def index():
     return render_template("index.html")
 
 
-
 @app.route('/login_signup/')
 def login_signup():
-
-
-
     if request.method == "POST":
         name = request.form.get("fname")
         email = request.form.get("emailid")
@@ -33,26 +28,38 @@ def login_signup():
 
         print(name, email, password)
         db.Quizs.insert_one({"name": name, "email": email, "password": password})
-    return render_template("index.html")
+    return render_template("login_signup.html")
 
-'''
-@app.route('/login_signup')
-def login():
-    users = my_client.db.Quiz
-    login_user = users.find_one({'email': request.form["username"]})
-
-    if login_user:
-        if bcrypt.hashpw(request.form["pass"].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
-            session['username'] = request.form['username']
-            return render_template("index.html")
-
-    return 'Invalid username/password combination'
-'''
 
 @app.route('/courses/')
-
 def courses():
     return render_template('courses.html')
+
+
+@app.route('/PythonQuiz/', methods=["GET", "POST"])
+def python():
+    return render_template('PythonQuiz.html')
+
+
+@app.route('/JavaQuiz/')
+def javaQuiz():
+    return render_template('corejavaquiz.html')
+
+
+@app.route('/JavaScriptQuiz/')
+def javaScriptQuiz():
+    return render_template('javascriptquiz.html')
+
+
+@app.route('/DataStructuresQuiz/')
+def DataStructuresQuiz():
+    return render_template('datastructuresquiz.html')
+
+
+@app.route('/CloudQuiz/')
+def CloudQuiz():
+    return render_template('cloudcomputingquiz.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
